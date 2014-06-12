953d3d94a692f0c53131bf4b11929365a3e04420
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-06-12 19:37:25 +0700

    Update ga examples

--- R/ga-data.R
+++ R/ga-data.R
@@ -30,8 +30,14 @@
 #' @name ga
 #'
 #' @examples
-#' # deprecated parameter was replaced by
+#' # a count of parameters types
+#' table(ga$type)
+#' # parameters groups
+#' table(ga$group)
+#' # get a deprecated parameters was replaced by
 #' subset(ga, status == "DEPRECATED", c(id, replacedBy))
+#' # get a calculation metrics
+#' subset(ga, !is.na(calculation), c(id, calculation))
 #' # get a not deprecated metrics from user group
 #' subset(ga, group == "User" & type == "METRIC" & status != "DEPRECATED", id)
 NULL
