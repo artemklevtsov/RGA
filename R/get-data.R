421bd1fc05c475244e2874165620b2f0f59c98a9
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-07-06 16:52:18 +0700

    Move test_query function

--- R/get-data.R
+++ R/get-data.R
@@ -18,3 +18,20 @@ get_data <- function(type, query, path, token, verbose = getOption("rga.verbose"
     data.json <- api_request(url, token = token, verbose = verbose)
     return(data.json)
 }
+
+#' @title Checking Google Analytics report query
+#'
+#' @param type character string including report type.
+#' @param query \code{GAQuery} class object including a request parameters.
+#' @param token \code{\link[httr]{Token2.0}} class object with a valid authorization data.
+#' @param verbose logical. Should print information verbose?
+#'
+#' @noRd
+#'
+test_query <- function(type, query, token, verbose = FALSE) {
+    if (verbose)
+        message("Check query...")
+    query$max.results <- 1L
+    data.json <- get_data(type = type, query = query, token = token, verbose = verbose)
+    return(data.json)
+}
