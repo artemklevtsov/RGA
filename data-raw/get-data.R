068f4150a974bb49b3142fe94b3df293564b1d4d
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-09-03 09:01:23 +0700

    Fix typos

--- data-raw/get-data.R
+++ data-raw/get-data.R
@@ -3,9 +3,9 @@ url <- "https://www.googleapis.com/analytics/v3/metadata/ga/columns"
 # Get JSON data
 data_json <- jsonlite::fromJSON(url)
 # Subsettings
-data_r <- .subset2(data.json, "items")
-id <- .subset2(data.r, "id")
-attributes <- .subset2(data.r, "attributes")
+data_r <- .subset2(data_json, "items")
+id <- .subset2(data_r, "id")
+attributes <- .subset2(data_r, "attributes")
 # Convert column types
 attributes  <- transform(attributes,
     allowedInSegments = as.logical(match(allowedInSegments, table = c(NA, "true")) - 1),
