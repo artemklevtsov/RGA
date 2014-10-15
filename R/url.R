c2476b21a5cb351b8478a16f392493e70f22bb85
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-10-15 09:54:35 +0700

    Fix #6

--- R/url.R
+++ R/url.R
@@ -2,12 +2,12 @@
 #' @include utils.R
 #'
 build_path <- function(x) {
-    stopifnot(inherits(x, "list"))
     x <- compact(x)
     params <- names(x)
     params <- gsub("\\.", "-", params)
     values <- as.vector(x, mode = "character")
     string <- paste(params, values, sep = "/", collapse = "/")
+    string <- sub("^/", "", string)
     string <- gsub("//", "/", string)
     return(string)
 }
@@ -16,7 +16,6 @@ build_path <- function(x) {
 #' @include utils.R
 #'
 build_query <- function(x) {
-    stopifnot(inherits(x, "list"))
     x <- compact(x)
     params <- names(x)
     params <- gsub("\\.", "-", params)
@@ -38,25 +37,21 @@ build_query <- function(x) {
 #'
 #' @noRd
 #'
-build_url <- function(type = c("ga", "mcf", "rt", "mgmt"), path, query) {
+build_url <- function(type = c("ga", "mcf", "rt", "mgmt"), path = NULL, query = NULL) {
     type <- match.arg(type)
-    base_url <- switch(type,
+    url <- switch(type,
                        ga = "https://www.googleapis.com/analytics/v3/data/ga",
                        mcf = "https://www.googleapis.com/analytics/v3/data/mcf",
                        rt = "https://www.googleapis.com/analytics/v3/data/realtime",
                        mgmt = "https://www.googleapis.com/analytics/v3/management",
                        stop("Unknown API data type."))
-    if (!missing(path)) {
-        if (length(path) > 1L)
-            path <- build_path(path)
-        if (nzchar(path))
-            url <- paste(base_url, path, sep = "/")
+    if (!is.null(path)) {
+        path <- build_path(path)
+        url <- paste(url, path, sep = "/")
     }
-    if (!missing(query)) {
-        if (length(query) > 1L)
-            query <- build_query(query)
-        if (nzchar(query))
-            url <- paste(base_url, query, sep = "?")
+    if (!is.null(query)) {
+        query <- build_query(query)
+        url <- paste(url, query, sep = "?")
     }
     return(url)
 }
