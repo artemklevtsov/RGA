0b968f0483ffc866679ca730043e710b400aeada
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-10-19 19:09:41 +0700

    Add csv data file

--- data-raw/get-data.R
+++ data-raw/get-data.R
@@ -17,6 +17,7 @@ attributes  <- transform(attributes,
 # Create data.frame
 ga <- cbind(id, attributes, stringsAsFactors = FALSE)
 # Save dataset
+write.csv(ga, file = "data-raw/ga.csv", row.names = FALSE)
 devtools::use_data(ga, internal = TRUE, overwrite = TRUE)
 # Resave sysdata.rda with the best compression
 tools::resaveRdaFiles(paths = "R/sysdata.rda")
