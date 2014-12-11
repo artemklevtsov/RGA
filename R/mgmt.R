2e5b5d0c3fa9dd8c3bbb4e00f8f239d0d0125eb6
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-12-12 00:26:21 +0700

    Add some get functions for the Management API

--- R/mgmt.R
+++ R/mgmt.R
@@ -23,7 +23,7 @@ get_mgmt <- function(path, query, token, verbose = getOption("rga.verbose", FALS
 #' @return An account collection provides a list of Analytics accounts to which a user has access. The account collection is the entry point to all management information. Each resource in the collection corresponds to a single Analytics account.
 #' \item{id}{Account ID.}
 #' \item{name}{Account name.}
-#' \item{permissions}{All the permissions that the user has for this account. These include any implied permissions (e.g., EDIT implies VIEW).}
+#' \item{permissions}{Permissions the user has for this account.}
 #' \item{created}{Time the account was created.}
 #' \item{updated}{Time the account was last modified.}
 #'
@@ -53,7 +53,7 @@ list_accounts = function(start.index = NULL, max.results = NULL, token, verbose 
 #' @param token \code{\link[httr]{Token2.0}} class object with a valid authorization data.
 #' @param verbose logical. Should print information verbose?
 #'
-#' @return web property collection lists Analytics web properties to which the user has access. Each resource in the collection corresponds to a single Analytics web property.
+#' @return A web property collection lists Analytics web properties to which the user has access. Each resource in the collection corresponds to a single Analytics web property.
 #' \item{id}{Web property ID of the form UA-XXXXX-YY.}
 #' \item{accountId}{Account ID to which this web property belongs.}
 #' \item{internalWebPropertyId}{Internal ID for this web property.}
@@ -63,7 +63,7 @@ list_accounts = function(start.index = NULL, max.results = NULL, token, verbose 
 #' \item{profileCount}{View (Profile) count for this web property.}
 #' \item{industryVertical}{The industry vertical/category selected for this web property.}
 #' \item{defaultProfileId}{Default view (profile) ID.}
-#' \item{permissions}{All the permissions that the user has for this web property. These include any implied permissions (e.g., EDIT implies VIEW) or inherited permissions from the parent account.}
+#' \item{permissions}{Permissions the user has for this web property.}
 #' \item{created}{Time this web property was created.}
 #' \item{updated}{Time this web property was last modified.}
 #'
@@ -85,10 +85,52 @@ list_webproperties = function(account.id = "~all", start.index = NULL, max.resul
     return(res)
 }
 
+#' @title Gets a web property to which the user has access to
+#'
+#' @param account.id integer or character. Account ID to retrieve the web property for.
+#' @param webproperty.id character. ID to retrieve the web property for.
+#' @param token \code{\link[httr]{Token2.0}} class object with a valid authorization data.
+#' @param verbose logical. Should print information verbose?
+#'
+#' @return An Analytics web property.
+#' \item{id}{Web property ID of the form UA-XXXXX-YY.}
+#' \item{kind}{Resource type for Analytics WebProperty.}
+#' \item{selfLink}{Link for this web property.}
+#' \item{accountId}{Account ID to which this web property belongs.}
+#' \item{internalWebPropertyId}{Internal ID for this web property.}
+#' \item{name}{Name of this web property.}
+#' \item{websiteUrl}{Website url for this web property.}
+#' \item{level}{Level for this web property.}
+#' \item{profileCount}{View (Profile) count for this web property.}
+#' \item{industryVertical}{The industry vertical/category selected for this web property.}
+#' \item{defaultProfileId}{Default view (profile) ID.}
+#' \item{permissions}{Permissions the user has for this web property.}
+#' \item{created}{Time this web property was created.}
+#' \item{updated}{Time this web property was last modified.}
+#' \item{parentLink}{Parent link for this web property. Points to the account to which this web property belongs.}
+#' \item{childLink}{Child link for this web property. Points to the list of views (profiles) for this web property.}
+#'
+#' @seealso \code{\link{authorize}}
+#'
+#' @references
+#' \href{https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/management/webproperties}{Google Management API - Web Properties}
+#'
+#' @family The Google Analytics Management API
+#'
+#' @include request.R
+#'
+#' @export
+#'
+get_webproperty <- function(account.id, webproperty.id, token, verbose = getOption("rga.verbose", FALSE)) {
+    path <- paste("accounts", account.id, "webproperties", webproperty.id, sep = "/")
+    res <- get_response(type = "mgmt", path = path)
+    return(res)
+}
+
 #' @title Lists views (profiles) which the user has access to
 #'
-#' @param account.id integer or character. Account ID for the view (profiles) to retrieve. Can either be a specific account ID or '~all', which refers to all the accounts which the user has access to. Requires specified account.id.
-#' @param webproperty.id character. Web property ID for the views (profiles) to retrieve. Can either be a specific web property ID or '~all', which refers to all the web properties which the user has access to.
+#' @param account.id integer or character. Account ID for the view (profiles) to retrieve. Can either be a specific account ID or '~all', which refers to all the accounts which the user has access to.
+#' @param webproperty.id character. Web property ID for the views (profiles) to retrieve. Can either be a specific web property ID or '~all', which refers to all the web properties which the user has access to. Requires specified \code{account.id}.
 #' @param start.index integer. An index of the first entity to retrieve. Use this parameter as a pagination mechanism along with the max-results parameter.
 #' @param max.results integer. The maximum number of views (profiles) to include in this response.
 #' @param token \code{\link[httr]{Token2.0}} class object with a valid authorization data.
@@ -108,7 +150,7 @@ list_webproperties = function(account.id = "~all", start.index = NULL, max.resul
 #' \item{stripSiteSearchCategoryParameters}{Whether or not Analytics will strip search category parameters from the URLs in your reports.}
 #' \item{excludeQueryParameters}{The query parameters that are excluded from this view (profile).}
 #' \item{eCommerceTracking}{Indicates whether ecommerce tracking is enabled for this view (profile).}
-#' \item{permissions}{All the permissions that the user has for this view (profile). These include any implied permissions (e.g., EDIT implies VIEW) or inherited permissions from the parent web property.}
+#' \item{permissions}{Permissions the user has for this view (profile).}
 #' \item{created}{Time this view (profile) was created.}
 #' \item{updated}{Time this view (profile) was last modified.}
 #'
@@ -130,6 +172,52 @@ list_profiles = function(account.id = "~all", webproperty.id = "~all", start.ind
     return(res)
 }
 
+#' @title Gets a view (profile) to which the user has access to
+#'
+#' @param account.id integer or character. Account ID to retrieve the goal for.
+#' @param webproperty.id character. Web property ID to retrieve the goal for.
+#' @param profile.id View (Profile) ID to retrieve the goal for.
+#' @param token \code{\link[httr]{Token2.0}} class object with a valid authorization data.
+#' @param verbose logical. Should print information verbose?
+#'
+#' @return An Analytics view (profile).
+#' \item{id}{View (Profile) ID.}
+#' \item{kind}{Resource type for Analytics view (profile).}
+#' \item{selfLink}{Link for this view (profile).}
+#' \item{accountId}{Account ID to which this view (profile) belongs.}
+#' \item{webPropertyId}{Web property ID of the form UA-XXXXX-YY to which this view (profile) belongs.}
+#' \item{internalWebPropertyId}{Internal ID for the web property to which this view (profile) belongs.}
+#' \item{name}{Name of this view (profile).}
+#' \item{currency}{The currency type associated with this view (profile).}
+#' \item{timezone}{Time zone for which this view (profile) has been configured. Time zones are identified by strings from the TZ database.}
+#' \item{websiteUrl}{Website URL for this view (profile).}
+#' \item{siteSearchQueryParameters}{The site search query parameters for this view (profile).}
+#' \item{stripSiteSearchQueryParameters}{Whether or not Analytics will strip search query parameters from the URLs in your reports.}
+#' \item{type}{View (Profile) type. Supported types: WEB or APP.}
+#' \item{permissions}{Permissions the user has for this view (profile).}
+#' \item{created}{Time this view (profile) was created.}
+#' \item{updated}{Time this view (profile) was last modified.}
+#' \item{eCommerceTracking}{Indicates whether ecommerce tracking is enabled for this view (profile).}
+#' \item{parentLink}{Parent link for this view (profile). Points to the web property to which this view (profile) belongs.}
+#' \item{childLink}{Child link for this view (profile). Points to the list of goals for this view (profile).}#'
+#'
+#' @seealso \code{\link{authorize}}
+#'
+#' @references
+#' \href{https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/management/profiles}{Google Management API - Unsampled Reports}
+#'
+#' @family The Google Analytics Management API
+#'
+#' @include request.R
+#'
+#' @export
+#'
+get_profile <- function(account.id, webproperty.id, profile.id, token, verbose = getOption("rga.verbose", FALSE)) {
+    path <- paste("accounts", account.id, "webproperties", webproperty.id, "profiles", profile.id, sep = "/")
+    res <- get_response(type = "mgmt", path = path)
+    return(res)
+}
+
 #' @title Lists goals which the user has access to
 #'
 #' @param account.id integer or character. The account ID to retrieve goals for. Can either be a specific account ID or '~all', which refers to all the accounts that user has access to.
@@ -144,6 +232,7 @@ list_profiles = function(account.id = "~all", webproperty.id = "~all", start.ind
 #' \item{id}{Goal ID (number).}
 #' \item{accountId}{Account ID which this goal belongs to.}
 #' \item{webPropertyId}{Web property ID which this goal belongs to. The web property ID is of the form UA-XXXXX-YY.}
+#' \item{internalWebPropertyId}{Internal ID for the web property to which this goal belongs.}
 #' \item{profileId}{View (Profile) ID to which this goal belongs.}
 #' \item{name}{Goal name.}
 #' \item{value}{Goal value.}
@@ -173,6 +262,49 @@ list_goals = function(account.id = "~all", webproperty.id = "~all", profile.id =
     path <- paste("accounts", account.id, "webproperties", webproperty.id, "profiles", profile.id, "goals", sep = "/")
     query <- list(start.index = start.index, max.results = max.results)
     res <- get_mgmt(path = path, query = query, token = token, verbose = verbose)
+    return(res)
+}
+
+#' @title Gets a goal to which the user has access to
+#'
+#' @param account.id integer or character. Account ID to retrieve the goal for.
+#' @param webproperty.id character. Web property ID to retrieve the goal for.
+#' @param profile.id ineger or character. View (Profile) ID to retrieve the goal for.
+#' @param goal.id ineger or character. Goal ID to retrieve the goal for.
+#' @param token \code{\link[httr]{Token2.0}} class object with a valid authorization data.
+#' @param verbose logical. Should print information verbose?
+#'
+#' @return An Analytics goal resource
+#' \item{id}{Goal ID.}
+#' \item{kind}{Resource type for an Analytics goal.}
+#' \item{selfLink}{Link for this goal.}
+#' \item{accountId}{Account ID to which this goal belongs.}
+#' \item{webPropertyId}{Web property ID to which this goal belongs. The web property ID is of the form UA-XXXXX-YY.}
+#' \item{internalWebPropertyId}{Internal ID for the web property to which this goal belongs.}
+#' \item{profileId}{View (Profile) ID to which this goal belongs.}
+#' \item{name}{Goal name.}
+#' \item{value}{Goal value.}
+#' \item{active}{Determines whether this goal is active.}
+#' \item{type}{Goal type. Possible values are URL_DESTINATION, VISIT_TIME_ON_SITE, VISIT_NUM_PAGES, and EVENT.}
+#' \item{created}{Time this goal was created.}
+#' \item{updated}{Time this goal was last modified.}
+#' \item{parentLink}{Parent link for a goal. Points to the view (profile) to which this goal belongs.}
+#' \item{visitTimeOnSiteDetails}{Details for the goal of the type VISIT_TIME_ON_SITE.}
+#'
+#' @seealso \code{\link{authorize}}
+#'
+#' @references
+#' \href{https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/management/goals}{Google Management API - Goals}
+#'
+#' @family The Google Analytics Management API
+#'
+#' @include request.R
+#'
+#' @export
+#'
+get_goal <- function(account.id, webproperty.id, profile.id, goal.id, token, verbose = getOption("rga.verbose", FALSE)) {
+    path <- paste("accounts", account.id, "webproperties", webproperty.id, "profiles", profile.id, "goals", goal.id, sep = "/")
+    res <- get_response(type = "mgmt", path = path)
     return(res)
 }
 
@@ -253,6 +385,53 @@ list_unsampled_reports <- function(account.id, webproperty.id, profile.id, start
     path <- paste("accounts", account.id, "webproperties", webproperty.id, "profiles", profile.id, "unsampledReports", sep = "/")
     query <- list(start.index = start.index, max.results = max.results)
     res <- get_mgmt(path = path, query = query, token = token, verbose = verbose)
+    return(res)
+}
+
+#' @title Getsa single unsampled report
+#'
+#' @param account.id integer or character. Account ID to retrieve unsampled report for.
+#' @param webproperty.id integer or character. Web property ID to retrieve unsampled reports for.
+#' @param profile.id integer or character. View (Profile) ID to retrieve unsampled report for.
+#' @param unsampled.report.id integer or character. Web property ID to retrieve unsampled reports for.
+#' @param token \code{\link[httr]{Token2.0}} class object with a valid authorization data.
+#' @param verbose logical. Should print information verbose?
+#'
+#' @return An Analytics unsampled report.
+#' \item{id}{Unsampled report ID.}
+#' \item{kind}{Resource type for an Analytics unsampled report.}
+#' \item{selfLink}{Link for this unsampled report.}
+#' \item{title}{Title of the unsampled report.}
+#' \item{accountId}{Account ID to which this unsampled report belongs.}
+#' \item{webPropertyId}{Web property ID to which this unsampled report belongs. The web property ID is of the form UA-XXXXX-YY.}
+#' \item{profileId}{View (Profile) ID to which this unsampled report belongs.}
+#' \item{start-date}{The start date for the unsampled report.}
+#' \item{end-date}{The end date for the unsampled report.}
+#' \item{metrics}{The metrics for the unsampled report.}
+#' \item{dimensions}{The dimensions for the unsampled report.}
+#' \item{filters}{The filters for the unsampled report.}
+#' \item{segment}{The segment for the unsampled report.}
+#' \item{status}{Status of this unsampled report.  Possible values are PENDING, COMPLETED, or FAILED.}
+#' \item{downloadType}{The type of download you need to use for the report data file.}
+#' \item{driveDownloadDetails}{Download details for a file stored in Google Drive.}
+#' \item{cloudStorageDownloadDetails}{Download details for a file stored in Google Cloud Storage.}
+#' \item{created}{Time this unsampled report was created.}
+#' \item{updated}{Time this unsampled report was last modified.}
+#'
+#' @seealso \code{\link{authorize}}
+#'
+#' @references
+#' \href{https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/management/unsampledReports}{Google Management API - Unsampled Reports}
+#'
+#' @family The Google Analytics Management API
+#'
+#' @include request.R
+#'
+#' @export
+#'
+get_unsampled_report <- function(account.id, webproperty.id, profile.id, unsampled.report.id, token, verbose = getOption("rga.verbose", FALSE)) {
+    path <- paste("accounts", account.id, "webproperties", webproperty.id, "profiles", profile.id, "unsampledReports", unsampled.report.id, sep = "/")
+    res <- get_mgmt(path = path, token = token, verbose = verbose)
     return(res)
 }
 
