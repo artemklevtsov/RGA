3215a7e093f9f8ba6aac32b639ce056f98f1f6e1
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-12-28 17:24:58 +0700

    Share api url and version

--- R/url.R
+++ R/url.R
@@ -27,6 +27,9 @@ build_query <- function(x) {
     return(string)
 }
 
+base_api_url <- "https://www.googleapis.com/analytics"
+base_api_version <- "v3"
+
 #' @title Build URL for Google Analytics request
 #'
 #' @param type type of Google Analytics API data.
@@ -41,11 +44,12 @@ build_query <- function(x) {
 #'
 build_url <- function(type = c("ga", "mcf", "rt", "mgmt"), path = NULL, query = NULL) {
     type <- match.arg(type)
-    url <- switch(type,
-                  ga = "https://www.googleapis.com/analytics/v3/data/ga",
-                  mcf = "https://www.googleapis.com/analytics/v3/data/mcf",
-                  rt = "https://www.googleapis.com/analytics/v3/data/realtime",
-                  mgmt = "https://www.googleapis.com/analytics/v3/management")
+    type <- switch(type,
+                  ga = "data/ga",
+                  mcf = "data/mcf",
+                  rt = "data/realtime",
+                  mgmt = "management")
+    url <- paste(base_api_url, base_api_version, type, sep = "/")
     if (!is.null(path)) {
         if (length(path) > 1)
             path <- build_path(path)
