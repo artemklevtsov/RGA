ed0244b015d13def5db37da594ca95ed50874eed
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-01-05 17:18:30 +0700

    Replace code of the retrieve data to RGA::list_metadata

--- data-raw/get-data.R
+++ data-raw/get-data.R
@@ -1,21 +1,5 @@
-# Request URL
-url <- "https://www.googleapis.com/analytics/v3/metadata/ga/columns"
-# Get JSON data
-data_json <- jsonlite::fromJSON(url)
-# Subsettings
-data_r <- .subset2(data_json, "items")
-id <- .subset2(data_r, "id")
-attributes <- .subset2(data_r, "attributes")
-# Convert column types
-attributes  <- transform(attributes,
-    allowedInSegments = as.logical(match(allowedInSegments, table = c(NA, "true")) - 1),
-    minTemplateIndex = as.numeric(minTemplateIndex),
-    maxTemplateIndex = as.numeric(maxTemplateIndex),
-    premiumMinTemplateIndex = as.numeric(premiumMinTemplateIndex),
-    premiumMaxTemplateIndex = as.numeric(premiumMaxTemplateIndex)
-)
-# Create data.frame
-ga <- cbind(id, attributes, stringsAsFactors = FALSE)
+# Get data
+ga <- RGA::list_metadata("ga")
 # Save dataset
 write.csv(ga, file = "data-raw/ga.csv", row.names = FALSE)
 devtools::use_data(ga, internal = TRUE, overwrite = TRUE)
