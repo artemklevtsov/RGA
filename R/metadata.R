d0e562861c83b70fd19f8e88acc128ae92b92736
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-01-27 21:00:41 +0700

    Return ga data object

--- R/metadata.R
+++ R/metadata.R
@@ -69,3 +69,58 @@ list_metadata <- function(report.type = "ga") {
     message(paste("Obtained data.frame with", nrow(data_df), "rows and", ncol(data_df), "columns."))
     return(data_df)
 }
+
+#' @title Lists all columns for a Google Analytics core report type
+#'
+#' @usage
+#' ga
+#'
+#' @description
+#' This dataset represents all of the dimensions and metrics for the reporting API with their attributes. Attributes returned include UI name, description, segments support, etc.
+#'
+#' @format
+#' A data frame with 434 rows and 14 variables containing the following columns:
+#' \describe{
+#' \item{id}{Parameter name.}
+#' \item{type}{The type of column: \code{DIMENSION}, \code{METRIC}.}
+#' \item{data.type}{The type of data this column represents: \code{STRING}, \code{INTEGER}, \code{PERCENT}, \code{TIME}, \code{CURRENCY}, \code{FLOAT}.}
+#' \item{group}{The dimensions/metrics group the column belongs to.}
+#' \item{status}{The status of the column: \code{PUBLIC}, \code{DEPRECATED}.}
+#' \item{ui.name}{The name/label of the column used in user interfaces (UI).}
+#' \item{description}{The full description of the column.}
+#' \item{allowed.in.segments}{Indicates whether the column can be used in the segment query parameter.}
+#' \item{replaced.by}{The replacement column to use for a column with a \code{DEPRECATED} status.}
+#' \item{calculation}{Only available for calculated metrics. This shows how the metric is calculated.}
+#' \item{min.template.index}{Only available for templatized columns. This is the minimum index for the column.}
+#' \item{max.template.index}{Only available for templatized columns. This is the maximum index for the column.}
+#' \item{premium.min.template.index}{Only available for templatized columns. This is the minimum index for the column for premium properties.}
+#' \item{premium.max.template.index}{Only available for templatized columns. This is the maximum index for the column for premium properties.}
+#' }
+#'
+#' @source \url{https://www.googleapis.com/analytics/v3/metadata/ga/columns?pp=1}
+#'
+#' @references
+#' \href{https://developers.google.com/analytics/devguides/reporting/metadata/v3/}{Google Analytics Metadata API}
+#'
+#' \href{https://developers.google.com/analytics/devguides/reporting/core/dimsmets}{Core Reporting API - Dimensions & Metrics Reference}
+#'
+#' @keywords data datasets
+#' @docType data
+#' @name ga
+#'
+#' @seealso \code{\link{get_ga}} \code{\link{dimsmets}} \code{\link{list_metadata}}
+#'
+#' @examples
+#' # a count of parameters types
+#' table(ga$type)
+#' # parameters groups
+#' table(ga$group)
+#' # get a deprecated parameters was replaced by
+#' subset(ga, status == "DEPRECATED", c(id, replaced.by))
+#' # get a calculation metrics
+#' subset(ga, !is.na(calculation), c(id, calculation))
+#' # get a not deprecated metrics from user group
+#' subset(ga, group == "User" & type == "METRIC" & status != "DEPRECATED", id)
+#' # get parameters allowed in segments
+#' subset(ga, allowed.in.segments, id)
+NULL
