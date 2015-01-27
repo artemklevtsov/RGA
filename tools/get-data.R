e4bd2d03d7b8a4e24e305026b4587e5b7feda776
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-01-27 20:57:18 +0700

    Move and rewrite get-data.R script

--- tools/get-data.R
+++ tools/get-data.R
@@ -2,6 +2,7 @@
 ga <- RGA::list_metadata("ga")
 # Save dataset
 write.csv(ga, file = "data-raw/ga.csv", row.names = FALSE)
-devtools::use_data(ga, internal = TRUE, overwrite = TRUE)
+devtools::use_data(ga, overwrite = TRUE)
 # Resave sysdata.rda with the best compression
-tools::resaveRdaFiles(paths = "R/sysdata.rda")
+for (f in list.files("data/", , full.names = TRUE))
+    tools::resaveRdaFiles(paths = f)
