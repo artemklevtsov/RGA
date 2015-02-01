6432c3b6fe4681f11c5a2a946434bd038361aee1
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-02-01 13:26:11 +0700

    Fix package docs

--- R/package.R
+++ R/package.R
@@ -11,7 +11,7 @@
 #'   \itemize{
 #'     \item \href{https://developers.google.com/analytics/devguides/config/mgmt/v3}{Management API}: access to configuration data for accounts, web properties, views (profiles), goals and segments;
 #'     \item \href{https://developers.google.com/analytics/devguides/reporting/core/v3}{Core Reporting API}: query for dimensions and metrics to produce customized reports;
-#'     \item {https://developers.google.com/analytics/devguides/reporting/mcf/v3}{Multi-Channel Funnels Reporting API}: query the traffic source paths that lead to a user's goal conversion;
+#'     \item \href{https://developers.google.com/analytics/devguides/reporting/mcf/v3}{Multi-Channel Funnels Reporting API}: query the traffic source paths that lead to a user's goal conversion;
 #'     \item \href{https://developers.google.com/analytics/devguides/reporting/realtime/v3}{Real Time Reporting API}: report on activity occurring on your property at the moment;
 #'     \item \href{https://developers.google.com/analytics/devguides/reporting/metadata/v3}{Metadata API}: access the list of API dimensions and metrics and their attributes;
 #'   }
@@ -21,6 +21,24 @@
 #' }
 #'
 #' To report a bug please type: \code{utils::bug.report(package = "RGA")}.
+#'
+#' @section Useage:
+#'
+#' Once you have the package loaded, there are 3 steps you need to use to get data from Google Analytics:
+#'
+#' \enumerate{
+#'   \item Authorize this package to access your Google Analytics data with the \code{\link{authorize}} function;
+#'   \item Determine the profile ID which you want to get access to with the \code{\link{list_profiles}} function;
+#'   \item Get the results from the API with one of these functions: \code{\link{get_ga}}, \code{\link{get_mcf}} or \code{\link{get_rt}}.
+#' }
+#'
+#' For details about this steps please type into R: \code{browseVignettes(package = "RGA")}
+#'
+#' @section Bug reports:
+#'
+#' Before posting a bug please try execute your code with the \code{\link[httr]{with_verbose}} wrapper. It will be useful if you attach verbose output to the bug report. For example: \code{httr::with_verbose(list_profiles())}
+#'
+#' To report a bug please type into R: \code{utils::bug.report(package = "RGA")}
 #'
 #' @author
 #' Artem Klevtsov \email{a.a.klevtsov@@gmail.com}
@@ -36,15 +54,15 @@
 #' # load package
 #' library(RGA)
 #' # get access token
-#' authorize(client.id = "myClientID", client.secret = "myClientSecret", cache = FALSE)
+#' authorize()
 #' # get a GA profiles
-#' ga_profiles <- get_profiles()
+#' ga_profiles <- list_profiles()
 #' # choose the profile ID by site URL
-#' profile.id <- ga_profiles[ga_profiles$websiteUrl == "mySiteURL", "id"]
+#' id <- ga_profiles[grep("example.com", ga_profiles$website.url), "id"]
 #' # get date when GA tracking began
-#' first.date <- firstdate(profile.id)
+#' first.date <- firstdate(id)
 #' # get GA report data
-#' ga_data <- get_ga(profile.id, start.date = first.date, end.date = "today",
+#' ga_data <- get_ga(id, start.date = first.date, end.date = "today",
 #'                   metrics = "ga:users,ga:sessions", dimensions = "ga:userGender,ga:userAgeBracket")
 #' }
 #'
