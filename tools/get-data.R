9d18652a3ef49b88a54f7b8e88f6d41e2f3db7d9
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-05-12 16:09:01 +0700

    Remove odd comma

--- tools/get-data.R
+++ tools/get-data.R
@@ -4,5 +4,5 @@ ga <- RGA::list_metadata("ga")
 write.csv(ga, file = "data-raw/ga.csv", row.names = FALSE)
 devtools::use_data(ga, overwrite = TRUE)
 # Resave sysdata.rda with the best compression
-for (f in list.files("data/", , full.names = TRUE))
+for (f in list.files("data/", full.names = TRUE))
     tools::resaveRdaFiles(paths = f)
