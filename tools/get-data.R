6c8a635e8de43863355d878498aeb69b082d30b4
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-06-13 13:24:21 +0700

    No need loop here

--- tools/get-data.R
+++ tools/get-data.R
@@ -4,5 +4,4 @@ ga <- RGA::list_metadata("ga")
 write.csv(ga, file = "data-raw/ga.csv", row.names = FALSE)
 devtools::use_data(ga, overwrite = TRUE)
 # Resave sysdata.rda with the best compression
-for (f in list.files("data/", full.names = TRUE))
-    tools::resaveRdaFiles(paths = f)
+tools::resaveRdaFiles(paths = "data/")
