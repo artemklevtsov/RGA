44de4900141f91f873fe2398d7aedce8a9436851
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-06-13 11:29:22 +0700

    Small updates for the vignettes

---
+++ vignettes/reporting-api.R
@@ -0,0 +1,8 @@
+## ---- echo = FALSE, message = FALSE--------------------------------------
+knitr::opts_chunk$set(
+    comment = "#>",
+    collapse = TRUE,
+    error = FALSE,
+    tidy = FALSE
+)
+
