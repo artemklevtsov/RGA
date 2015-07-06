e56bef775969a180c77902974dca0646106afeb2
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2015-07-06 11:21:25 +0700

    Add some comments to the get_data

--- R/get-data.R
+++ R/get-data.R
@@ -2,6 +2,7 @@
 #' @include request.R
 get_data <- function(type = c("ga", "rt", "mcf", "mgmt"), path = NULL, query = NULL, token) {
     type <- match.arg(type)
+    # Set limits
     if (type == "mgmt") {
         results_limit <- 1000L
         items_name <- "items"
@@ -16,15 +17,18 @@ get_data <- function(type = c("ga", "rt", "mcf", "mgmt"), path = NULL, query = N
         pagination <- FALSE
         stopifnot(query$max.results <= results_limit)
     }
+    # Add fields
     if (is.null(query$fields))
         query$fields <- paste("totalResults", items_name, sep = ",")
     else
         query$fields <- paste("totalResults", query$fields, sep = ",")
+    # Make request
     data_json <- get_response(type = type, path = path, query = query, token = token)
     if (data_json$totalResults == 0L || is.null(data_json[[items_name]]) || length(data_json[[items_name]]) == 0L)
         return(NULL)
     if (!isTRUE(pagination) && query$max.results < data_json$totalResults)
         warning(paste("Only", query$max.results, "observations out of", data_json$totalResults, "were obtained. Set max.results = NULL (default value) to get all results."), call. = FALSE)
+    # Pagination
     if (isTRUE(pagination) && query$max.results < data_json$totalResults) {
         if (type == "rt")
             warning(paste("Only", query$max.results, "observations out of", data_json$totalResults, "were obtained (the batch processing mode is not implemented for this report type)."), call. = FALSE)
