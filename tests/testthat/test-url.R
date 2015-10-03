6350b9955d465f2f87acf70b5c9336b7e5f3d41e
Artem Klevtsov <a.a.klevtsov@gmail.com>
2015-10-03 17:13:58 +0700

    Fix test for the get_url

--- tests/testthat/test-url.R
+++ tests/testthat/test-url.R
@@ -1,6 +1,6 @@
 context("API request URL")
 
-url <- get_url(type = "mgmt", path = "accounts")
+url <- get_url(path = c("management", "accounts"), query = list(start.index = 1, max.results = 1000))
 
 test_that("URL class", {
     expect_is(url, "character")
@@ -11,6 +11,8 @@ test_that("URL length", {
 })
 
 test_that("URL match", {
+    expect_match(url, "www.googleapis.com/analytics")
     expect_match(url, "management")
     expect_match(url, "accounts")
+    expect_match(url, "start-index")
 })
