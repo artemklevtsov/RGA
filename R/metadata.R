f21e5556c88e915a2f8ad113e78d3ffac2c660f0
Artem Klevtsov <a.a.klevtsov@gmail.com>
2015-10-19 09:55:50 +0700

    Improve type convert for the metadata API

--- R/metadata.R
+++ R/metadata.R
@@ -56,7 +56,7 @@ list_dimsmets <- function(report.type = "ga") {
     res <- data_$items
     res$kind <- NULL
     colnames(res) <- gsub("attributes.", "", colnames(res), fixed = TRUE)
-    res$allowed.in.segments <- ifelse(res$allowed.in.segments == "true", TRUE, FALSE)
+    res$allowed.in.segments <- type.convert(toupper(res$allowed.in.segments))
     return(res)
 }
 
