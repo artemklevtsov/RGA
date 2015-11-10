4681b742035cb734786975019572646ead57884d
Artem Klevtsov <a.a.klevtsov@gmail.com>
2015-11-11 01:04:32 +0700

    Update get-docs.R

--- tools/get-docs.R
+++ tools/get-docs.R
@@ -3,9 +3,14 @@ library(jsonlite)
 library(stringr)
 library(magrittr)
 
-urls <- read_html("https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/") %>%
+api_methods <- c("list", "get")
+
+base_url <- "https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/"
+
+urls <- read_html(base_url) %>%
     html_nodes("a") %>% html_attr("href") %>%
-    str_subset("https://.*/mgmtReference/management")
+    str_subset("#resource") %>% str_replace_all("#resource", "") %>%
+    str_c("https://developers.google.com", .)
 
 to_separated <- function(x, sep = ".") {
     x <- gsub("PropertyId", "propertyId", x, fixed = TRUE)
@@ -14,50 +19,64 @@ to_separated <- function(x, sep = ".") {
     gsub("([[:lower:]])([[:upper:]])", paste0("\\1", sep, "\\L\\2"), x, perl = TRUE)
 }
 
-get_returns <- function(url, output.dir = ".") {
-    res <- read_html(url)
-    pre <- res %>% html_nodes("#alt-json") %>%
-        html_text(trim = TRUE) %>%
-        str_replace_all(" (\\w+)", " \"\\1\" ") %>%
-        fromJSON()
-    prop_names <- unique(c(names(pre), names(unlist(pre)))) %>%
-        to_separated()
-    tbl <- res %>% html_nodes("#properties") %>%
-        html_table(fill = TRUE) %>%
-        extract2(1)
-    tbl$`Property name` %<>% str_replace_all("\\[\\]", "") %>% to_separated()
+get_return <- function(x) {
+    pre <- x %>% html_nodes("pre#alt-json") %>% html_text(trim = TRUE) %>%
+        str_replace_all(" (\\w+)", " \"\\1\" ") %>% fromJSON()
+    prop_names <- unique(c(names(pre), names(unlist(pre))))
+    tbl <- x %>% html_nodes("table#properties") %>%
+        html_table(fill = TRUE) %>% extract2(1)
+    tbl$`Property name` %<>% str_replace_all("\\[\\]", "")
     tbl <- tbl[match(prop_names, tbl$`Property name`), ]
-    txt <- sprintf("#' \\item{%s}{%s}", tbl$`Property name`, tbl$Description)
-    file_name <- url %>% str_split("/|#") %>%
-        unlist %>% extract(11) %>%
-        str_c("-resource.txt")
-    message("Writing ", file_name, "...")
-    writeLines(txt, file.path(output.dir, file_name))
+    title <- x %>% html_nodes("section#overview p") %>% html_text(trim = TRUE) %>% extract(1)
+    title <- sprintf("#' @return %s\n", title)
+    items <- sprintf("#' \\item{%s}{%s}\n", tbl$`Property name` %>% to_separated(), tbl$Description)
+    c(title, items)
 }
 
-get_params <- function(url, output.dir = ".") {
-    tbl <- read_html(url) %>%
-        html_nodes("#request_parameters") %>%
-        html_table(fill = TRUE) %>%
-        extract2(1) %>%
-        na.omit()
-    tbl$`Parameter name` %<>% to_separated()
-    txt <- sprintf("#' @param %s %s", tbl$`Parameter name`, tbl$Description)
-    file_name <- url %>% str_split("/|#") %>%
-        unlist %>%  extract(c(11, 12)) %>%
-        str_c(collapse = "-") %>% str_c(".txt")
-    message("Writing ", file_name, "...")
-    writeLines(txt, file.path(output.dir, file_name))
+get_title <- function(x) {
+    title <- x %>% html_nodes("h1.devsite-page-title") %>% html_text(trim = TRUE) %>%
+        str_split(":") %>% unlist %>% extract(1)
+    sprintf("#' @title %s\n", title)
 }
 
-if (!file.exists("tmp/"))
-    dir.create("tmp/")
+get_methods <- function(x) {
+    methods <- x %>% html_nodes("section#methods dt") %>% html_text()
+    desc <- x %>% html_nodes("section#methods dd") %>% html_text()
+    txt <- sprintf("#' @description %s\n", desc)
+    names(txt) <- methods
+    return(txt)
+}
 
-idx <- grep("get|list|insert|delete|patch|update|uploadData|deleteUploadData", urls)
+get_params <- function(x) {
+    tbl <- x %>% html_nodes("table#request_parameters") %>%
+        html_table(fill = TRUE) %>% extract2(1) %>% na.omit()
+    tbl$Value %<>% str_replace_all("string", "character")
+    tbl$`Parameter name` %<>% to_separated() %>% tolower()
+    params <- sprintf("#' @param %s %s. %s\n", tbl$`Parameter name`, tbl$Value, tbl$Description)
+    c(params, "#\' @param token \\code{\\link[httr]{Token2.0}} class object with a valid authorization data.\n")
+}
 
-urls[-idx] %>%
-    lapply(get_returns, output.dir = "tmp/") %>%
-    invisible()
-urls[idx] %>%
-    lapply(get_params, output.dir = "tmp/") %>%
-    invisible()
+get_man <- function(url) {
+    html <- read_html(url)
+    nm <- url %>% str_split("/management/") %>% unlist() %>% extract(2) %>% to_separated(sep = "_")
+    title <- get_title(html)
+    values <- get_return(html)
+    descs <- get_methods(html)
+    family <- "#' @family Management API"
+    ref <- sprintf("#' @references \\href{%s}{Management API - %s Overview}\n", url, title %>% str_replace_all("#' @title |\n", ""))
+    methods <- descs %>% names() %>% str_subset(api_methods %>% str_c(collapse = "|"))
+    for (m in methods) {
+        if (m == "list")
+            values %<>% extract(values %>% str_detect("self.link|parent.link|child.link") %>% not())
+        url2 <- str_c(url, m, sep = "/")
+        html2 <- read_html(url2)
+        desc <- descs[m]
+        params <- get_params(html2)
+        fn <- str_c(m, nm, sep = "_") %>% str_c(".R")
+        message("Writing ", fn, "...")
+        cat(title, desc, str_c(params, collapse = ""), str_c(values, collapse = ""), ref, family,
+            sep = "#' \n", file = file.path("man-roxygen", fn))
+    }
+}
+
+for (url in urls %>% extract(urls %>% str_detect("Links$") %>% not())) get_man(url)
