c73dbaee4ac5c9dc644ad49f6daa309f0c04453b
Artem Klevtsov <a.a.klevtsov@gmail.com>
2015-11-12 17:14:13 +0700

    Improve api_request and  process_response

--- R/request.R
+++ R/request.R
@@ -19,16 +19,27 @@ error_reasons <- function(x) {
 #' @include utils.R
 process_response <- function(response) {
     stopifnot(inherits(response, "response"))
-    res <- jsonlite::fromJSON(httr::content(response, as = "text"), flatten = TRUE)
     if (response$status_code == 404L)
         stop(sprintf("The requested URL not found. URL: %s.", strsplit(url, "?", fixed = TRUE)[[1L]][1L]), call. = FALSE)
-    if (!is.null(res$error))
-        stop(error_reasons(res), call. = FALSE)
-    res <- convert_types.list(res)
-    res <- convert_names(res)
-    idx <- sapply(res, is.list)[!grepl("^(rows|items)$", names(res))]
-    res[idx] <- lapply(res[idx], convert_types.list)
-    res[idx] <- lapply(res[idx], convert_names)
+    if (response$status_code == 204L)
+        return(NULL)
+    if (httr::http_status(response)$category == "success") {
+        res <- jsonlite::fromJSON(httr::content(response, as = "text"), flatten = TRUE)
+        res <- convert_types.list(res)
+        res <- convert_names(res)
+        idx <- sapply(res, is.list)[!grepl("^(rows|items)$", names(res))]
+        res[idx] <- lapply(res[idx], convert_types.list)
+        res[idx] <- lapply(res[idx], convert_names)
+    } else {
+        type <- httr::parse_media(response$headers$`Content-type`)
+        if (type$complete == "application/json") {
+            res <- jsonlite::fromJSON(httr::content(response, as = "text"))
+            stop(error_reasons(res), call. = FALSE)
+        } else {
+            res <- httr::content(response, as = "text")
+            stop(sprintf("There has been an error, the request no succeeded. HTTP error: %s.", res), call. = FALSE)
+        }
+    }
     return(res)
 }
 
@@ -43,13 +54,13 @@ api_request <- function(url, token) {
         config <- httr::config(token = token)
     for (i in 0L:6L) {
         response <- httr::GET(url, config = config, httr::accept_json())
-        res <- try(process_response(response), silent = TRUE)
-        if (!inherits(res, "try-error"))
+        res <- tryCatch(process_response(response), error = function(e) e)
+        if (!inherits(res, "simpleError"))
             break
-        else if (grepl("User rate limit exceeded|Quota exceeded", res) & i < 6L)
+        else if (grepl("User rate limit exceeded|Quota exceeded", res$message) & i < 6L)
             Sys.sleep(2L^i + stats::runif(1L))
         else
-            stop(res, call. = FALSE)
+            stop(res)
     }
     return(res)
 }
