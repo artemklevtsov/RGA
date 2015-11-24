cb9d0b0bb570528cb8281b099f2a746c5a0e0532
Artem Klevtsov <a.a.klevtsov@gmail.com>
2015-11-24 22:28:46 +0700

    No need to set rga.username here

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,6 @@
 .onLoad <- function(libname, pkgname) {
     op <- options()
     op.rga <- list(
-        rga.username = NULL,
         rga.client.id = "144394141628-8m5i5icva7akegi3tp6215d9eg9o5cln.apps.googleusercontent.com",
         rga.client.secret = "wlFmhluHqTdZw6UG22h5A2nr",
         rga.cache = ".ga-token.rds"
