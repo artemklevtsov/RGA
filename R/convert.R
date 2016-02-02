219873343c3ab59daa727111e3535e1375d791e2
Artem Klevtsov <a.a.klevtsov@gmail.com>
2016-02-03 03:32:40 +0700

    Unnest in the mgmt_df

--- R/convert.R
+++ R/convert.R
@@ -16,15 +16,15 @@ collapse_mcf <- function(x) {
 # Build data.frame for mcf report
 mcf_df <- function(x) {
     col_names <- x$columnHeaders$name
-    idx <- grep("MCF_SEQUENCE", x$columnHeaders$dataType, fixed = TRUE)
-    if (length(idx)) {
-        primitive <- lapply(x$rows, function(i) .subset2(i, "primitiveValue")[-idx])
+    ind <- grep("MCF_SEQUENCE", x$columnHeaders$dataType, fixed = TRUE)
+    if (length(ind)) {
+        primitive <- lapply(x$rows, function(i) .subset2(i, "primitiveValue")[-ind])
         primitive <- do.call(rbind, primitive)
-        colnames(primitive) <- col_names[-idx]
-        conversion <- lapply(x$rows, function(i) .subset2(i, "conversionPathValue")[idx])
+        colnames(primitive) <- col_names[-ind]
+        conversion <- lapply(x$rows, function(i) .subset2(i, "conversionPathValue")[ind])
         conversion <- lapply(conversion, collapse_mcf)
         conversion <- do.call(rbind, conversion)
-        colnames(conversion) <- col_names[idx]
+        colnames(conversion) <- col_names[ind]
         res <- data.frame(primitive, conversion, row.names = NULL, stringsAsFactors = FALSE)[, col_names]
     } else {
         res <- as.data.frame(do.call(rbind, unlist(x$rows, recursive = FALSE, use.names = FALSE)), stringsAsFactors = FALSE)
@@ -36,12 +36,21 @@ mcf_df <- function(x) {
 # Build data.frame for mgmt data
 mgmt_df <- function(x) {
     res <- x$items
-    idx <- grep("Link|kind", colnames(res))
-    if (length(idx))
-        res <- res[-idx]
+    res <- res[!grepl("Link|kind", colnames(res))]
     if (!is.null(res$permissions.effective)) {
         names(res) <- gsub(".effective", "", names(res), fixed = TRUE)
         res$permissions <- vapply(res$permissions, paste, collapse = ",", FUN.VALUE = character(1))
+    }
+    ind <- vapply(res, is.list, logical(1))
+    if (any(ind)) {
+        nrows <- nrow(res)
+        unnest <- function(x) {
+            x[lengths(x) == 0] <- NA
+            do.call(rbind, x)
+        }
+        res <- c(res[!ind], unlist(lapply(res[ind], unnest), recursive = FALSE))
+        class(res) <- "data.frame"
+        row.names(res) <- seq_len(nrows)
     }
     return(res)
 }
