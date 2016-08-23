c0f91a102ef50c57289ac5fb9bda1ef5bc95d920
Artem Klevtsov <a.a.klevtsov@gmail.com>
2016-08-23 12:21:36 +0700

    Fix R CMD check warnings

--- R/remarketingAudience.R
+++ R/remarketingAudience.R
@@ -3,15 +3,15 @@
 #' @export
 get_remarketing_audience <- function(accountId, webPropertyId, remarketingAudienceId, token) {
     path <- sprintf("management/accounts/%s/webproperties/%s/remarketingAudiences/%s",
-                    accountId, webPropertyId, customDimensionId)
+                    accountId, webPropertyId, remarketingAudienceId)
     get_mgmt(path, token)
 }
 
 #' @template list_remarketingAudience
 #' @include mgmt.R
 #' @export
-list_remarketing_audiences <- function(accountId, webPropertyId, start.index = NULL, max.results = NULL, token) {
+list_remarketing_audiences <- function(accountId, webPropertyId, type = "", start.index = NULL, max.results = NULL, token) {
     path <- sprintf("management/accounts/%s/webproperties/%s/remarketingAudiences",
                     accountId, webPropertyId)
-    list_mgmt(path, list(start.index = start.index, max.results = max.results), token)
+    list_mgmt(path, list(type = type, start.index = start.index, max.results = max.results), token)
 }
